2d2de9f60ccb886cf752d6ae2843e08e43d0e3f3
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 20:16:46 +0500

    stringni numberga utqazish

---
+++ 3.dars.js/3.javascript.js
@@ -0,0 +1,21 @@
+// ========================[
+ // stringni numberga utqazish//
+// =========1
+// let a="10.3"
+// console.log(+a)
+// ===========
+
+// ========2
+// let b="10"
+// console.log(Number(b));
+// ==========
+
+// =========3 kasir kiremas
+// let b="10.3"
+// console.log(parseInt(b))
+// =============
+
+// =======4
+// let b="10.6"
+// console.log(parseFloat(b))
+// =============]
