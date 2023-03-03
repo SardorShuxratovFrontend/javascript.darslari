9badb8fd4b19aa126b3a5b449cb6b47792d9bf5f
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 20:57:23 +0500

    AND &&

---
+++ 7.dars.js/7.javascript.js
@@ -0,0 +1,20 @@
+ // AND &&  ========[ falsega ishledi
+// =============== AND &&
+// ikalasi true busa true qaytaradi
+// let id = true;
+// let face =true;
+// console.log(id && face);//true
+//============ 0, null, indefined.
+//bitasi folse busayam false qaytaradi
+// let id =false;
+// let face = true;
+// console.log(id && face)//false
+// console.log(true && false);//false
+// console.log(''&& 0 && true &&false);//
+// console.log('' && 0 && true && false);
+// console.log(0 && true);//0
+// console.log("sardor" && null);//null
+// console.log(null && undefined);//null
+// console.log(false && null);//false
+// console.log( false  || undefined);//undefined
+//================================]
