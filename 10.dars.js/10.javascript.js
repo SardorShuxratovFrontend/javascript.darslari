dddb1277194d7514050593e76c656a4c47b9e854
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 22:37:00 +0500

    if else

---
+++ 10.dars.js/10.javascript.js
@@ -0,0 +1,61 @@
+//==================================[
+          //  if else //
+  //if doyim true, false ga ishledi
+  //   let temp = 1;
+  //   if (true) {
+  //     console.log("Havo issiq");
+  //   }
+  //   // if, ta  0,  null, undefined <false digani// 
+  //  if (false) {
+  //    console.log("Havo sovuq");
+  // }
+ 
+  //========================
+      //can open gmail chiqadi shu teng busa 18 >= 18
+//  let age = prompt()
+//  if(age >= 18){
+//   console.log("can open gmail")
+//  }else{
+//   console.log("can.t open gmail");
+//  }
+//===========================
+ // can.t open gmail chiqadi, >=  ,quymasak 18 kichkina bupqoladi  //
+//  let age1 = prompt()
+//  if(age > 18){
+//   console.log("can open gmail")
+//  }else{
+//   console.log("can.t open gmail");
+//  }
+//============================
+// |else if| nima uchun kire? - tugri javob topildimi undan 
+//  qoganini uqib utirmedi.
+//============================ 
+// agar |if| uzini yozib kitadigan buse uzidan 
+// kiyingilarinyam uqib kitadi u  tugri buladimi 
+// notug'ri buladimi farqi yuq uqidi.
+   //else if  ishlash holatti//
+// let temp = "sovuq";
+// if (temp === "issiq") {
+//   console.log("havo issiq yengilrok keyining");
+// }
+// else if (temp === "sovuq") {
+//   console.log("Havo sovuq qalinroq kiyinib oling");
+// }
+// else if (temp === "yomgrli") {
+//   console.log("Havo yomgirli soyabon oling");
+// }
+// else if (temp === "qor") {
+//   console.log("Havo qorli qalinroq kiyinib oling");
+// }
+// else{
+//   console.log("nomalum obi havo");
+// }
+//======Condishinal operator ? :========
+// ? if digani,// : else digani//
+// let temp = 25; // havo issiq
+// temp > 20;
+// ? console.log("havo issiq")
+// : console.log("nomalum ob havo");
+//======================================
+
+//====================================] 
