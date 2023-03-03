cf0c4505074e1bd191dbc49a516758812bbb556b
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 22:44:58 +0500

    For loop

---
+++ 12.dars.js/12.javascript.js
@@ -0,0 +1,33 @@
+        // For loop // zinadan chiqish//
+  //1,2,3,4,5,6,7,8,9,10 chiqadi |10 chi zinaga chiqish
+  // let i = 1;
+  // for (i=1; i <= 10; i++ ) {
+  //   console.log(i);
+  // }
+  //===================
+  //10,9,8,6,5,4,3,2,1 chiqadi |10 chi zinadan tushish
+  // let i=10;
+// for (i=10; i >= 1; i--)
+// console.log(i);
+
+  //===================
+  //tinimsiz 1 chiqaradi, i++ quymasak
+  // let i= 1;
+  // for (i=1; i<10; ){
+  // console.log(i);
+  // }
+  //===================
+  //for di ichiga let berilsa shundagina for ichidegi log ishledi//
+// for(let i = 1; i <= 10; i++){
+//   console.log(i);//< for ichida  ishledi
+// }
+// console.log(i);// for dan tashqarda ishlamedi hato beradi
+//=====================
+//1,3,6,10 javobi 10 tuxtedi
+// var sum = 0;
+// for (i = 1; i < 12; i++ ){
+// console.log((sum += i));
+// if ( sum === 10 )
+// break;;//tuxtash 10 buganda tuxtedi
+// }
+//====================]
