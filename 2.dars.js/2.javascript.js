eaa061f2dcea2afee95a7fe99b6c53d013968fdf
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 20:04:06 +0500

    data taype

---
+++ 2.dars.js/2.javascript.js
@@ -0,0 +1,74 @@
+//======== Data Taypes =================[
+        //primitive//
+
+//======string `` "" ''=====[
+// let lop = "`sardor`";|" ` ` "
+// let bob = "'zur'";   |" ' ' "
+// let name ='"salom"'; |' " "  '
+// console.log('sunnat  sardor   "javlon"' + lop + "it center")
+// console.log("sunnat  sardor    'bobur'")
+// console.log(`webbrien academy ${lop + name}  ${lop} ${bob}`)
+// ================================]
+
+//====== Number ==== raqam =[
+  // let a = 12;
+  // console.log(a)
+  //============]
+
+  //==== Symbol ===== yunik  id berish uchun ishlatiladi =[
+  // symbolda ikta birhil narsa busayam teng bumedi//
+// let a = Symbol("text")
+// let b = Symbol("text")
+// console.log(a == b);//false
+// console.log(a === b);//false
+//=======================]
+
+//======= Boolean =======[
+// true = 1;  //teng
+// false = 0; //teng 
+// console.log(true == false);//false
+// console.log(true == true )//true
+// console.log(false == false)//true
+//================]
+
+// ====bigInt == katta bugan sonlani ishlatib beradi[
+//  console.log(9007199254740992n+1n)
+// =============]
+
+// =======undefinet== shikalatam yoq qog'aziyam yuq[
+// let text; 
+// console.log(text);
+// =====
+// text= " salom";
+// console.log(text)
+// ===========]
+//=========================]
+
+//==== null== qog'ozi bor shikalt yoq =[
+// let a = null;
+// console.log(a);
+//============]
+
+//non primitive//
+
+//===== objekt ======[
+//======object,array,obj,faction; 
+// let ary=["Muxxamad","sardor","sunnat"];
+// let obj={name:"sador", status:"web brien"};
+//==================
+// ========factioni ishlatish
+// faction(){}
+// function test(){
+//     console.log(ary[0])
+//     console.log(obj.status)
+// }
+// test()
+// =====================
+// ======^=======
+// let array=[]
+// let obj={}
+// let fuct=(){}
+// function()=>{}
+//===============]
+ 
+//==========================================]
