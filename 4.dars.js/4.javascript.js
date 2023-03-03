d58700323f763533cbfb17c8b8b49301bf49f6e6
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 20:35:47 +0500

    Basic operators match

---
+++ 4.dars.js/4.javascript.js
@@ -0,0 +1,70 @@
+// =======================[
+
+     //Basic operators, match//
+// =========|+=|1 kushish
+//let a = 50;
+// a = a + 10; <asil holata yozilshi//50+10 
+//a+=10 <kiskardmasi
+//console.log(a)
+// ===============
+
+// ==========|-=|2 ayirish
+//let a = 50;
+//a = a-10 <asil holati //50-10
+//a-=10 //<kiskartma holati
+//console.log(a)
+// ============
+
+// =========|/=|3 bulish
+//let a = 10;
+//a = a / 5  //<asil holati//10/5
+//a/=5  //<kiskartma holati
+//console.log(a)
+// ==========
+
+// ========|%|koldig
+//let a = 55;
+//a = a % 10;//<asil holati  //55%10
+//a %= 10 //<kiskartma holati
+//console.log(a)
+// ==========
+
+// ========|*=| kupaytiru
+// let a = 5;
+// a = a * 5 //5*5 //<asil holati
+// a *= 5  //<kiskartma holati
+// console.log(a)
+//==================
+
+//===|**|nechinchidyr darajasi==//
+// console.log(2*2);//2**2 darajasi
+// console.log(2*2*2);//2**3 darajasi
+// console.log(2*2*2*2);//2**4 darajasi
+// console.log(2*2*2*2*2);//2**5 darajasi
+//===========================
+
+// ==============]
+
+// increment//
+//  let a = 11;
+//  console.log(a++)//11
+//  console.log(a++)//12
+//  console.log(++a)//14
+//  console.log(a++)//14
+//  console.log(++a)//16
+//  console.log(a++)//16
+//  console.log(a++)//17
+//  console.log(a)//18
+// ===========]
+
+// decrement //
+// let a = 11;
+// console.log(--a);//10
+// console.log(a--);//10
+// console.log(--a);//8
+// console.log(--a);//7
+// console.log(a--);//7
+// console.log(++a);//7
+//=======================
+
+//====================================]
