4c8d0cb6d52d251f7278afbeb78a6f161cfcf283
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 20:50:31 +0500

    OR ||

---
+++ 6.dars.js/6.javascript.js
@@ -0,0 +1,12 @@
+  //  OR ========[ truega ishledi
+//============ || <or belgisi// <ikalasini tekshirib beradi
+// let ceo = false;
+// let admin = true;
+// console.log( true || false)// true
+// console.log("" || null);//null
+// console.log("" || undefined);//undefined
+// console.log(false || "sardor");//sardor
+// console.log(true || "sardor");//true
+// console.log("sardor" || "sunnat");//sardor
+// console.log("" || "begi");//begi
+//==========================]
