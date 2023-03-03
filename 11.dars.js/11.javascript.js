b6651edfa4a6b53bd4ce29e36a8021a475ea39e4
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-03 22:41:31 +0500

    Switch statement

---
+++ 11.dars.js/11.javascript.js
@@ -0,0 +1,13 @@
+
+
+// Switch statement// if ga uxshab ishledi //
+ //  Switch pakat qatii tenglikta ishledi//
+ // switch - if ti switchtan  farqi qatii tenglikta ishledi-
+ // if esa qatii bumasayam ishloradi-
+    //  let data = "qor"
+    //  switch (data) {
+    //  case "yomgir":
+    //  console.log("bugin yomgir");
+    //  break;//tuxtedi stop digani
+    //  default: console.log("nomalum ob havo");// hammasi hato busa shunga kiladi//
+    //}
