0086e06ff788e0d7dd597572b1ccf8711710d767
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-05 03:05:13 +0500

    While,Do while

---
+++ 13dars.js/13.javascript.js
@@ -0,0 +1,39 @@
+       // while,do while//
+  //=0,null,undefined ,false yurmedi,=//
+  //=while qachonki |true| busa ushada ishledi=//
+  //  while (0) {
+  //  console.log("hey");//hishnarsa chiqmedi
+  // }
+//===============
+
+//=cheksiz davom itadi=//
+// while (true ) {
+//   console.log("hey");
+// }
+//=================
+
+//=tepadan boshlab sanab kiladi 3 2 1 console da=//
+  // let  i = 3;
+  // while (i) {
+  //   alert(i)
+  //   i--;
+  // }
+  //==================
+
+  // i=3
+  // while (i) {
+  //   console.log(`hey`);
+  //   i--;
+  // }
+
+  // do{
+  //   console.log(i);
+  //   i--
+  // }while(i==0)
+
+  //javobi//
+    //hey
+    //hey
+    //hey
+    //0
+//===============
