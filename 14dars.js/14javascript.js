e17ec493b807a6fbd7d5e2984433d47a15bcb02a
SardorShuxratovDeveloper <sardorshuxratov2005@gmail.com>
2023-03-14 06:48:00 +0500

    obvjekt

--- 14dars.js/14javascript.js
+++ 14dars.js/14javascript.js
@@ -1,42 +1,24 @@
-
-          // Object : ikki nuxtablam yoziladi //
-// objekt nima ? - key va valydan tashkil topkan bir malumotka aytiladi//
-       // primitiv valyu  qaytaradi
-       //non promotiv manzilini qaytaradi
-       // let account = "webbrain";
-       // console.log(account);
-
-    //=========
-       //Objektka kiymat biriktirish//
-      //  let account = { 
+ //////////////////////////
+      //  let obj = { 
       //   name:"sardor",
       //   major:"frontent",
-      //   founded:2020
+      //   founded:2020  
       //  };
-      // console.log(account);//hamma malomoti okob beradi
-    //==========
+      //  obj.aeg =132213//aeg objectka kushilib koladi
+      // console.log(obj);
+   ///////////////////////////
+   
+   ////////////////////////////////////
+  // daynamek usulda set qilish //
+// let  name ="sardor" ;
+// let surname =" shuxartov";
+// let job ="title";
 
+// let obj = {
+//   name:"sardor",
+//   surname:"it center",
+//  [ title] : "eshmat",//shet uzida skop kuyipkitse buladi
+// };
 
-   //===========
-   //== bush Objekt kilish busak==//
-      // let newObj1={};
-      // let newobj2 = new Object();
-      // console.log(newObj1,newobj2);
-      // javobi
-      // {}
-      // ()
-   //===============
-
-   //===accssesing key ======//
-
-  //   let account  = {
-  //   name:"Webbrain Acedemy",
-  //   major:"frontent",
-  //   foundent:2020.,
-  //   "full name":"sardorbek",//ikki bug'inli malumot biriktirganda ishlatiladi
-  //   0: 1390,
-  //  };
-  //   console.log(account.name);//ecses qilish yulari 1
-  //   console.log(account ["full name"]);//ikki bug'inli malumot biriktirganda ishlatiladi 2
-  //   console.log([0]);//raqam orqali keganda shu  holda ecses buladi
-  //==========================
+// console.log(obj["title"]);
+//////////////////////////////////////
